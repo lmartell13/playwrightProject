1fca95da6b7b96ed5ec09fcf793da6734a8cccd1
lmartell13 <amartell259@gmail.com>
2025-11-08 12:16:53 -0600

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,34 +5,35 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   testDir: './tests',
+
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
   workers: process.env.CI ? 1 : undefined,
+
   reporter: [['html'], ['list']],
+
   use: {
-    baseURL: 'https://practicesoftwaretesting.com',
+    baseURL: process.env.URL ?? 'https://practicesoftwaretesting.com',
     trace: 'on',
     headless: true
   },
 
   projects: [
-    // Setup project: runs only locally to generate storageState.json
     {
       name: 'setup',
       testMatch: /.*auth\.setup\.ts/,
       grepInvert: process.env.CI ? /./ : undefined
     },
 
-    // Tests WITHOUT authentication (clean session)
     {
       name: 'chromium-login',
       testMatch: /tests\/login\/.*\.ts/,
+      grepInvert: process.env.CI ? /./ : undefined,
       use: {
         ...devices['Desktop Chrome']
       }
     },
 
-    // Tests WITH authentication (using saved storageState.json)
     {
       name: 'chromium-checkout',
       testMatch: /tests\/checkout\/.*\.ts/,
@@ -43,5 +44,4 @@ export default defineConfig({
       dependencies: process.env.CI ? [] : ['setup']
     }
   ]
-
 });
